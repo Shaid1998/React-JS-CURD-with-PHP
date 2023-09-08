58f5e86735acd7df8211c65265192915907a65cb
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-08 14:20:00 +0600

    Backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Create from './pages/Create';
+import { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Container fluid={true}>
+        <nav className='navbar navbar-expend navbar-dark bg-dark'>
+          <div className='navbar-nav'>
+            <li className='nav-item'>
+              <Link to={'/'} className='nav-link' >Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to={'/create'} className='nav-link' >Create</Link>
+            </li>
+          </div>
+        </nav>
+        <div className='container'>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/create" element={<Create />}></Route>
+          </Routes>
+        </div>
+      </Container>
+    </Fragment>
   );
 }
 
