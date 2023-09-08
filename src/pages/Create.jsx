28862ef966ca2bfa714f48d6deb83806e5bdfb21
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-08 16:47:34 +0600

    Crud Oparetion

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import http from '../http'
+import { useNavigate } from 'react-router';
 
-class Create extends Component {
-  render() {
-    return (
-      <div>Create</div>
-    )
+export default function Create(){
+  const navigate = useNavigate();
+  const [inputs , setInputs] = useState([]);
+  const handleChange = (event)=> {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values=>({...values,[name]:value}))
   }
-}
+  const SubmitForm = () =>{
+    http.post('/users',inputs).then((res)=>{
+      navigate('/');
+    })
+  }
+  return(
+    <div>
+      <h2>New User</h2>
+      <div className="row">
+        <div className="col-sm-6 justify-content-center">
+          <div className="card p-4">
+            <label>Name</label>
+            <input type="text" name="name" className="form-control" value={inputs.name || ''} onChange={handleChange} />
 
-export default Create
+            <label>Email</label>
+            <input type="email" name="email" className="form-control" value={inputs.email || ''} onChange={handleChange} />
+
+            <label>Password</label>
+            <input type="password" name="password" className="form-control" value={inputs.password || ''} onChange={handleChange} />
+
+            <button type="button" onClick={SubmitForm} className="btn btn-info mt-2" >Create</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,41 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import http from '../http'
+export default function Home() {
+    const [users , setUsers] = useState([]);
+    useEffect(()=>{
+        fetchAllUsers();
+    },[])
 
-class Home extends Component {
-  render() {
+    const fetchAllUsers=()=>{
+        http.get('users').then(res=>{
+            setUsers(res.data);
+        })
+    }
     return (
-      <div>Home</div>
+        <div>
+            <h1>Table Listing </h1>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Serial</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((users,index)=>
+                        <tr key={users.id}>
+                            <td>{++index}</td>
+                            <td>{users.name}</td>
+                            <td>{users.email}</td>
+                            <td>edit</td>
+                        </tr>
+                    )}
+                    
+                </tbody>
+            </table>
+        </div>
     )
-  }
 }
 
-export default Home
